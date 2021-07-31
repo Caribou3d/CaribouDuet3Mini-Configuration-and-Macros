4b2c1bc9059453592244f05d1e0f5877a511df75
wschadow <wolfgang.schadow@t-online.de>
2021-08-01 00:28:19 +0200

    corrected definition of PINDA, updated CC-build to produce a whole SD-card

--- Configuration/sys/variants/Caribou220-LGX-NP.sh
+++ Configuration/sys/variants/Caribou220-LGX-NP.sh
@@ -88,7 +88,7 @@ sed -i "
 ; PINDA2 \\
 ;\\
 M558 P5 C\"^io1.in\" H1.5 F600 T8000 A3 S0.03             ; set z probe to PINDA2\\
-M308 S2 P"temp2" A\"Pinda V2\" Y\"thermistor\" T100000 B3950   ; temperature of PINDA2\\
+M308 S2 P\"temp2\" A\"Pinda V2\" Y\"thermistor\" T100000 B3950   ; temperature of PINDA2\\
 M557 X23:235 Y5:186 S30.25:30                               ; define mesh grid
 };
 {/#CARIBOU_OFFSETS/ c\
